f9eef6a1772f3b26ac8121410d40a72b69f5be3e
KAEL VANBREDA-FOXWELL <102675853@student.swin.edu.au>
2019-03-14 17:27:20 +1100

    Random.cs

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{			
+			string Input;
+			float real = 0;
+			int notReal = 0;
+			int count = 0;
+			
+
+			while(count <100){
+				count = count +1;
+				notReal = count;
+				real = count;
+				
+				if(real/3 == notReal/3 & real/5 == notReal/5){
+					Console.WriteLine("FizzBuzz");					
+				}else if(real/5 == notReal/5){
+					Console.WriteLine("Buzz");
+				}else if(real/3 == notReal/3){
+					Console.WriteLine("Fizz");
+				}else{
+					Console.WriteLine(count);
+				}
+			}
+			
+			Console.ReadKey(true);
+		}
+	}
+}
