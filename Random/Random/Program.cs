a756aa1cfeed47a9b9f92cab4d6af4cc4b17e2f5
KAEL VANBREDA-FOXWELL <102675853@student.swin.edu.au>
2019-03-14 17:06:28 +1100

    challange 6

--- Random/Random/Program.cs
+++ Random/Random/Program.cs
@@ -1,22 +1,40 @@
-﻿/*
- * Created by SharpDevelop.
- * User: 102675853
- * Date: 14/03/2019
- * Time: 4:22 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 
-namespace Random
+namespace Randoms
 {
 	class Program
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
 			
-			// TODO: Implement Functionality Here
+			int count = 0;
+			int left;
+			int numberInput = 0;
+			string input;
+			Random thisIsRandom = new Random();
+			int randomInt = thisIsRandom.Next(1, 1000);
+			
+			
+			while(count <10) {
+				
+				Console.WriteLine("Guess the secret number!");
+				input = Console.ReadLine();
+				numberInput = int.Parse(input);
+				
+				if (numberInput == randomInt) {
+					Console.WriteLine("NICE!");
+					count = 11;
+				}else{
+					count = count + 1;
+					left = 10 - count;
+					Console.WriteLine("you have " + left + " guesses left");
+					if(numberInput < randomInt) {
+						Console.WriteLine("Guess higher");
+					}else if (numberInput > randomInt) {
+						Console.WriteLine("Guess lower");
+					}
+				}				
+			}
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
